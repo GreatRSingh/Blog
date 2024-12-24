d9eeb714bdd4969f0f9a55ca964a43dcfbe0dd94
GreatRSingh <rakshitsingh421@gmail.com>
2024-12-24 21:22:55 +0530

    remove dist and make it auto generate

--- build.ts
+++ build.ts
@@ -1,9 +1,20 @@
-import fs from "fs";
+import fs from 'fs';
 import PostPage, {generateStaticParams} from "./components/post-page";
 import HomePage from "./components/index-page";
 
+
+// Building Folder Structure
+fs.mkdirSync('dist')
+fs.mkdirSync('dist/posts')
+
+// Copy the sitemap and style
+fs.copyFileSync('sitemap.txt', 'dist/sitemap.txt');
+fs.copyFileSync('style.css', 'dist/style.css');
+
+
 const posts = generateStaticParams()
 
+// Generate post pages
 for (let i = 0; i < posts.length; i++) {
     const post_html = PostPage(posts[i].slug)
     fs.writeFile('dist/posts/' + posts[i].slug +'.html', post_html, (e) => {
@@ -11,6 +22,7 @@ for (let i = 0; i < posts.length; i++) {
     })
 }
 
+// Generate home page
 const index_html = HomePage()
 fs.writeFile('dist/' +'index.html', index_html, (e) => {
     console.log(e)
